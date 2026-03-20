c2bd11133746cab42102394d0bb46493313a106f
BharatSharma29 <bharat429sharma@gmail.com>
2026-03-20 14:34:44 +0000

    ci/cd testing

--- frontend/src/components/VehicleForm.jsx
+++ frontend/src/components/VehicleForm.jsx
@@ -133,7 +133,7 @@ function VehicleForm({ fetchVehicles }) {
                     required
                 />
 
-                <button type="submit">Add Vehicle</button>
+                <button type="submit">Add Wehicle</button>
 
             </form>
 
